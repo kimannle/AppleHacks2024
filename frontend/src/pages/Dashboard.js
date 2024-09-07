4d56aa4368ff44e656c677f2f6082909173461e7
Damian Phimister <minemuppet@gmail.com>
2024-09-07 16:56:06 -0400

    Button Difference

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import '../styles/Dashboard.css';
+import { Link } from 'react-router-dom';
+
 
 function Dashboard() {
   const username = 'John Doe';
   const email = 'john.doe@example.com';
 
+  const background = 'linear-gradient(180deg, #000000 0%, #27082c 50%, #51115b 75%, #ca7ed6 100%)';
+  const glow = '0 0 30px 10px rgba(65, 11, 102, 0.8)';
+
   return (
     <div className="dashboard-container">
       {/* Display username and email */}
       <div className="user-info">
-        <p>{username}</p>
-        <p>{email}</p>
+        <p>Hello, {username}!</p>
       </div>
 
       {/* Centered circle with buttons */}
-      <div className="circle-container">
-        <div className="circle">
-          <button className="dashboard-button">Activity</button>
-          <button className="dashboard-button">Progress</button>
-        </div>
+      <div className="circle">
+        <span>See Progress</span>
+      </div>
+      <div className="circle" style={{ background: background, boxShadow: glow }}>
+        <span>Daily Tasks</span>
       </div>
     </div>
   );
