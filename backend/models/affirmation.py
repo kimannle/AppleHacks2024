45d93bbcb5a14905711c6d8aa27059670012639c
Dakotah Jones <152076033+Dakmaster@users.noreply.github.com>
2024-09-08 00:09:45 -0400

    Create completed task tables (#5)
    * Renamed tables to completedActivities & completedAffirmations
    
    Fixed a single space in activity.py for formatting purposes
    
    Setting up completedActivities & completedAffirmations
    Finishing up what columns should be added
    Essentially formatted from the other tables
        for reference ^^^
    
    * Attempted to create foreign keys for the new table
    only reason is because ID is used in each table
    Not sure if this will work properly
    
    * Removed status from activity.py and affirmation.py
    Need to test if it is working properly, and values are associated
    accordingly.

--- backend/models/affirmation.py
+++ backend/models/affirmation.py
@@ -4,9 +4,8 @@ from shared import BASE
 # THIS IS READ ONLY, FOR STORAGE OF THE STRINGS
 
 class Affirmation(BASE):
+
     __tablename__ = "Affirmation"
 
     id = Column(Integer, primary_key=True, nullable=False)
     affirmation = Column(String(250), nullable=False)
-    status = Column(Boolean, nullable=False)
-    
