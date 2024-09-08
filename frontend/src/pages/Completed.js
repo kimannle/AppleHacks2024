58dd2ae451df94b7a34f4e26f66af4b5bb83f950
Damian Phimister <minemuppet@gmail.com>
2024-09-08 11:52:59 -0400

    Finished up completed page

--- frontend/src/pages/Completed.js
+++ frontend/src/pages/Completed.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import "../styles/Completed.css"
+import { useNavigate } from 'react-router-dom';
 
 function Completed() {
+
+    const navigate = useNavigate();
+
+    const handleButtonClick= () => {
+        navigate('/dashboard');
+    };
+
     return (
-        <div className="progress-container">
+        <div className="completed-container">
             <h1>You did it!</h1>
 
             <div>
                 <p>Let's go back to the dashboard, so we can see your progress!</p>
             </div>
-            <button>Dashboard</button>
+            <button onClick={handleButtonClick}>Dashboard</button>
         </div>
     );
 }
