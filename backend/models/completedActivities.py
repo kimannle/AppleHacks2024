6ab50dd03a2c7dd73ca03c88ee29e20ee0620b74
Dakotah Jones <dakmasterr@gmail.com>
2024-09-07 23:57:58 -0400

    Attempted to create foreign keys for the new table only reason is because ID is used in each table Not sure if this will work properly

--- backend/models/completedActivities.py
+++ backend/models/completedActivities.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from shared import BASE
 
 # will be location of completed user activities
@@ -8,4 +8,5 @@ class CompletedActivities(BASE):
     __tablename__ = "CompletedActivities"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    
+    activity_id = Column(Integer, ForeignKey('Activity.id'), nullable=False)
+    status = Column(Boolean, nullable=False)
--- backend/models/completedAffirmations.py
+++ backend/models/completedAffirmations.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from shared import BASE
 
 # will be location of completed user affirmations
@@ -8,4 +8,5 @@ class CompletedAffirmations(BASE):
     __tablename__ = "CompletedAffirmations"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    
+    affirmation_id = Column(Integer, ForeignKey('Affirmation.id'), nullable=False)
+    status = Column(Boolean, nullable=False)
