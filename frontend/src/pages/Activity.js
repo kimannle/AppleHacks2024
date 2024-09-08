836c1bbca22efda56b473c5f3294ac036fa96208
Kimberly Le <lekimberly022@gmail.com>
2024-09-08 00:39:47 -0400

    wrap fetch call for random activity in useEffect

--- frontend/src/pages/Activity.js
+++ frontend/src/pages/Activity.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/AcAv.css';
 
 function Activity() {
     const [activity, setActivity] = useState("");
-    fetch("/daily-activity", {
-        method: 'GET'
-    }).then(
-        res => res.json()
-    ).then(
-        data => {
-            console.log(data);
-            setActivity(data.activity);
-        }
-    ).catch(
-        error => console.error('Error:', error)
-    );
+
+    useEffect(() => {
+        fetch("/daily-activity", {
+            method: 'GET'
+        }).then(
+            res => res.json()
+        ).then(
+            data => {
+                console.log(data);
+                setActivity(data.activity);
+            }
+        ).catch(
+            error => console.error('Error:', error)
+        );
+    }, []);
 
     return (
         <div className="ac-av">
-            <h2> The universe has spoken forth today’s fated task!</h2>
+            <h2>The universe has spoken forth today’s fated task!</h2>
             <p>{activity}</p>
             <button>I did it!</button>
         </div>
-    )
+    );
 }
 
 export default Activity;
