bfb98416ad4d0c380471ec9e11995b94e2f9d7d7
Damian Phimister <minemuppet@gmail.com>
2024-09-08 00:33:55 -0400

    Activity and DB

--- frontend/src/pages/Activity.js
+++ frontend/src/pages/Activity.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/AcAv.css';
 
 function Activity() {
+    const [activity, setActivity] = useState("");
+    fetch("/daily-activity", {
+        method: 'GET'
+    }).then(
+        res => res.json()
+    ).then(
+        data => {
+            console.log(data);
+            setActivity(data.activity);
+        }
+    ).catch(
+        error => console.error('Error:', error)
+    );
+
     return (
         <div className="ac-av">
             <h2> The universe has spoken forth today’s fated task!</h2>
-            
+            <p>{activity}</p>
             <button>I did it!</button>
         </div>
     )
