2f28cbb3e1a4a439bef4fffa397730a1dd407d90
Kimberly Le <lekimberly022@gmail.com>
2024-09-08 02:22:44 -0400

    Hooked up the affirmation component to fetch a random affirmation of the day

--- frontend/src/pages/Affirmation.js
+++ frontend/src/pages/Affirmation.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/AcAv.css';
 
 function Affirmation() {
-    return (
-        <div>
+    const [affirmation, setAffirmation] = useState("");
 
+    useEffect(() => {
+        fetch("/daily-affirmation", {
+            method: 'GET'
+        }).then(
+            res => res.json()
+        ).then(
+            data => {
+                console.log(data);
+                setAffirmation(data);
+            }
+        ).catch(
+            error => console.error('Error:', error)
+        );
+    }, []);
+
+    return (
+        <div className="ac-av">
+            <h2>The universe has a reminder for you!</h2>
+            <p>{affirmation}</p>
+            <button>I did it!</button>
         </div>
-    )
+    );
 }
 
 export default Affirmation;
